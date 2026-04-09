233fcef436dbc82156f8ba8a80aa8b2586b5c15b
Laiba Nasir <laibanasir@Mac.lan>
2026-04-08 22:20:52 -0700

    donegit add .

--- d_flip_flop.sv
+++ d_flip_flop.sv
@@ -5,5 +5,17 @@ module d_flip_flop(
 );
 
 // Put your implementation of a halfadder goes here
-
+// based on nandlands video: 
+/*
+always rising edege of clock
+* copy the posusion of the data on Q [the output]
+* keep the Q / output the same until u see the next rising ede 
+* then copy the next rising edge's instantaous slope
+* contonue this cycle
+*/
+always @(posedge Clk) begin
+    // copy the posusion of the data on Q [the output]
+    Q <= D; 
+    
+end
 endmodule
